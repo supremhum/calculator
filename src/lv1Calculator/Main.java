d602eebd1944e2bdbeac854cc1a6f8c076c5fe5d
supremhum <kalystols@gmail.com>
2025-02-26 15:06:16 +0900

    상황 설명 완료 종료입력까지 동작 구현 시작

--- src/lv1Calculator/Main.java
+++ src/lv1Calculator/Main.java
@@ -12,11 +12,6 @@ public class Main {
         String temp; // 버퍼 비우는용도
         Scanner scanner = new Scanner(System.in);
 
-        // 예외처리 1 : 숫자입력 원하는 타입이 아닌경우
-        // 예외처리 2 : 나눗셈 분모 0으로 가지 않기
-
-        // 예외처리 1 : 입력받을때 다른타입이면 오류가 뜨니 오류를 감지하는 try catch 구문을 사용
-        // 생각할거리 : 개행이 있는 상태로 입력을 받을수도 있다. 그러면 모든 버퍼를 지워야 한다
         while (true) {
             try {
                 System.out.printf("숫자 숫자 연산기호를 입력해 주세요 (계산결과는 정수형입니다) : ");
@@ -25,17 +20,28 @@ public class Main {
                 operator = scanner.nextLine().charAt(1);
                 switch (operator) {
                     case '+' :
-                        System.out.println(leftValue + rightValue);
+                        System.out.println(leftValue +" + "+ rightValue+" = "+(leftValue + rightValue));
                         break;
                     case '-' :
-                        System.out.println(leftValue - rightValue);
+                        System.out.println(leftValue +" - "+ rightValue+" = "+(leftValue - rightValue));
                         break;
                     case '*' :
-                        System.out.println(leftValue * rightValue);
+                        System.out.println(leftValue +" * "+ rightValue+" = "+(leftValue * rightValue));
                         break;
                     case '/' :
                         try{
-                            System.out.println(leftValue / rightValue);
+                            System.out.printf(leftValue +" / "+ rightValue+" 의 몫 = ");
+                            if (leftValue<0) {
+                                if (rightValue<0){
+                                    System.out.println((leftValue / rightValue)+1);
+                                }
+                                else {
+                                    System.out.println((leftValue / rightValue) - 1);
+                                }
+                            }
+                            else {
+                                System.out.println((leftValue / rightValue));
+                            }
                             break;
                         } catch (Exception e) {
                             System.out.println("분모에 0이 들어올 수 없습니다.");
@@ -48,7 +54,7 @@ public class Main {
                 }
                 break;
             } catch (Exception e) {
-                System.out.println("잘못 입력하셨습니다");
+                System.out.println("숫자를 잘못 입력하셨습니다");
                 temp = scanner.nextLine();
                 // 버퍼 비우기. 입력을 다시 받아야 하기 때문이고. 숫자에서 에러나면 뒤의 개행을 지워야 하기 때문
                 // 그런데 만약 엔터로 점철된 입력이였을 수도 있으니
