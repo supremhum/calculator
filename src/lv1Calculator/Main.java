9a885c487a2ded3e8fd26aff285600fa2d99d795
supremhum <kalystols@gmail.com>
2025-02-26 14:10:39 +0900

    입력 자체 오류 예외처리와 생각할거리

--- src/lv1Calculator/Main.java
+++ src/lv1Calculator/Main.java
@@ -9,32 +9,47 @@ public class Main {
         //입력을 받는다. 이 입력을 공백을 포함해서 받을건가? 아니면 하나하나 따로따로 받을건가? 공백으로 받자.
         long leftValue, rightValue; // 받을 숫자 롱타입
         char operator;
-        String temp;
+        String temp; // 버퍼 비우는용도
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("숫자 숫자 연산기호를 입력해 주세요 (계산결과는 정수형입니다) : ");
 
-        leftValue = scanner.nextLong();
-        rightValue = scanner.nextLong();
-//        operator = scanner.next(); // 공백이후 문자열을 받을 수 있으고 이후 띄어쓰기부터 엔터까진 버퍼에 남는상태
-        operator = scanner.nextLine().charAt(1);// 숫자를 받았고 버퍼엔 {space}연산기호{space}~라서 1번째인 연산기호를 저장
-//        scanner.nextLine(); // 버퍼비움. 콘솔에 입력후 엔터치면 {enter}=\n 까지 버퍼에 들어간다. 이 {enter}까지 흡수해버리는 코드
-//        현 상황에선 없어도 된다 오히려 콘솔창에 엔터입력을 해야돼서 느려짐
+        // 예외처리 1 : 숫자입력 원하는 타입이 아닌경우
+        // 예외처리 2 : 나눗셈 분모 0으로 가지 않기
 
-        switch (operator) {
-            case '+' :
-                System.out.println(leftValue + rightValue);
+        // 예외처리 1 : 입력받을때 다른타입이면 오류가 뜨니 오류를 감지하는 try catch 구문을 사용
+        // 생각할거리 : 개행이 있는 상태로 입력을 받을수도 있다. 그러면 모든 버퍼를 지워야 한다
+        while (true) {
+            try {
+                System.out.printf("숫자 숫자 연산기호를 입력해 주세요 (계산결과는 정수형입니다) : ");
+                leftValue = scanner.nextLong();
+                rightValue = scanner.nextLong();
+                operator = scanner.nextLine().charAt(1);
+                switch (operator) {
+                    case '+' :
+                        System.out.println(leftValue + rightValue);
+                        break;
+                    case '-' :
+                        System.out.println(leftValue - rightValue);
+                        break;
+                    case '*' :
+                        System.out.println(leftValue * rightValue);
+                        break;
+                    case '/' :
+                        System.out.println(leftValue / rightValue);
+                        break;
+                    default:
+                        System.out.println("잘못된 연산 입력입니다. (+,-,*,/)");
+//                        temp = scanner.nextLine(); 연산기호 받는 부분은 후순위고 nextLine 이므로 버퍼가 비워졌으니 필요없음
+                        continue;
+                }
                 break;
-            case '-' :
-                System.out.println(leftValue - rightValue);
-                break;
-            case '*' :
-                System.out.println(leftValue * rightValue);
-                break;
-            case '/' :
-                System.out.println(leftValue / rightValue);
-                break;
-            default:
-                System.out.println("잘못된 입력입니다.");
+            } catch (Exception e) {
+                System.out.println("잘못 입력하셨습니다");
+                temp = scanner.nextLine();
+                // 버퍼 비우기. 입력을 다시 받아야 하기 때문이고. 숫자에서 에러나면 뒤의 개행을 지워야 하기 때문
+                // 그런데 만약 엔터로 점철된 입력이였을 수도 있으니
+                // 반복문으로 수정할 필요 있음!
+
+            }
         }
         scanner.close(); // 스캐너는 닫아주는게 예의
     }
